cd8feb871bbab073025748dbbd9d59a70ca86b2d
psieg <padi@padimail.de>
2015-07-22 16:57:50 +0200

    add platform distinction

--- Software/scripts/win32/prepare_installer.sh
+++ Software/scripts/win32/prepare_installer.sh
@@ -6,6 +6,12 @@ mkdir -p dist_windows/content/translations
 cp res/translations/*.qm dist_windows/content/translations
 mkdir -p dist_windows/content/platforms
 cp $QTDIR/plugins/platforms/qwindows.dll dist_windows/content/platforms/
-#FIXME: decide origin by bitness
-#cp $WINDIR/System32/msvcr120.dll dist_windows/content/
-#cp $WINDIR/System32/msvcp120.dll dist_windows/content/
+# This assumes the build happens on a x64 machine
+if [ "$1" = "x86" ]
+then
+	cp $WINDIR/SysWOW64/msvcr120.dll dist_windows/content/
+	cp $WINDIR/SysWOW64/msvcp120.dll dist_windows/content/
+else
+	cp $WINDIR/System32/msvcr120.dll dist_windows/content/
+	cp $WINDIR/System32/msvcp120.dll dist_windows/content/
+fi

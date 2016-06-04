c7b63287fefb89d9cac632e61366d9274f345569
psieg <padi@padimail.de>
2016-06-04 19:59:29 +0200

    Add version to output filename

--- Firmware/build_batch.sh
+++ Firmware/build_batch.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
+VERSION=6+1
+
 if [ ! -e hex ]; then
 	mkdir hex
 fi
 
 make LIGHTPACK_HW=4
-mv Lightpack_hw4.hex hex/
+mv Lightpack_hw4.hex hex/Lightpack_hw4_${VERSION}.hex
 make clean LIGHTPACK_HW=4
 make LIGHTPACK_HW=5
-mv Lightpack_hw5.hex hex/
+mv Lightpack_hw5.hex hex/Lightpack_hw5_${VERSION}.hex
 make clean LIGHTPACK_HW=5
 make LIGHTPACK_HW=6
-mv Lightpack_hw6.hex hex/
+mv Lightpack_hw6.hex hex/Lightpack_hw6_${VERSION}.hex
 make clean LIGHTPACK_HW=6
 make LIGHTPACK_HW=7
-mv Lightpack_hw7.hex hex/
+mv Lightpack_hw7.hex hex/Lightpack_hw7_${VERSION}.hex
 make clean LIGHTPACK_HW=7

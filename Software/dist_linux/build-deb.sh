89e5e0f62bf180755b143a4e083d1e40d8a37b40
Patrick Siegler <github@psieg.de>
2016-07-03 18:02:37 +0200

    Replace deprecated md5deep

--- Software/dist_linux/build-deb.sh
+++ Software/dist_linux/build-deb.sh
@@ -30,12 +30,12 @@ then
 fi
 
 
-if [ -x "`which md5deep 2>/dev/null`" ]; 
+if [ -x "`which hashdeep 2>/dev/null`" ]; 
 then
 	# Update MD5 sums
-	md5deep -r deb/usr > deb/DEBIAN/md5sums
+	hashdeep -r deb/usr > deb/DEBIAN/md5sums
 else
-	echo "Please install 'md5deep' package first."
+	echo "Please install 'hashdeep' package first."
 	exit 5
 fi
 

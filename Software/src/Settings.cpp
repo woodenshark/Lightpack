7517aa8b8f95f7395e01d7cbd683b42a3c3610b3
asgardc3r <33607176+asgardc3r@users.noreply.github.com>
2017-12-16 13:40:55 +0100

    Update Settings.cpp

--- Software/src/Settings.cpp
+++ Software/src/Settings.cpp
@@ -816,8 +816,6 @@ QStringList Settings::getSupportedSerialPortBaudRates()
     QStringList list;
 
     // TODO: Add more baud rates if need it
-    list.append("2000000"); 
-    list.append("1500000");
     list.append("115200");
     list.append("57600");
     list.append("9600");

84ca5f19be80493644c5a190aec05f92b824e4ce
zomfg <sergio.kunats@gmail.com>
2018-12-22 22:41:56 +0100

    blue light reduction class now with virtual destructor and forced isSupported() override

--- Software/grab/include/BlueLightReduction.hpp
+++ Software/grab/include/BlueLightReduction.hpp
@@ -7,7 +7,9 @@ namespace BlueLightReduction
 	class Client
 	{
 	public:
+		static bool isSupported() { assert(("BlueLightReduction::isSupported() is not implemented", false)); return false; }
 		virtual void apply(QList<QRgb>& colors, const double gamma = 1.2) = 0;
+		virtual ~Client() = default;
 	};
 
 	Client* create();

81fed6f3bca54e4d43aad73413e027fb8d5f25c5
Patrick Siegler <github@psieg.de>
2019-01-17 20:43:31 +0100

    Force vs2017 for UpdateElevate

--- Software/scripts/win32/build_UpdateElevate.bat
+++ Software/scripts/win32/build_UpdateElevate.bat
@@ -1,6 +1,2 @@
-IF NOT "%VS120COMNTOOLS%" == "" (
-	call "%VS120COMNTOOLS%\vsvars32.bat"
-) ELSE (
-	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-)
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 MSBuild.exe UpdateElevate\UpdateElevate.sln /p:Configuration=Release

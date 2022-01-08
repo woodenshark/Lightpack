5044e21400cb1f8709cf121c6025138a60d4d044
Patrick Siegler <github@psieg.de>
2022-01-08 14:08:54 +0100

    Bump to 5.11.2.30

--- Software/src/version.h
+++ Software/src/version.h
@@ -27,10 +27,10 @@
 #pragma once
 
 // Current version of Lightpack software
-#define VERSION_STR "5.11.2.30"
+#define VERSION_STR "5.11.2.31"
 
 // Resource defines using in Lightpack.rc
-#define VER_FILEVERSION				5,11,2,30
+#define VER_FILEVERSION				5,11,2,31
 #define VER_PRODUCTVERSION			VER_FILEVERSION
 
 #define VER_FILEVERSION_STR			VERSION_STR "\0"

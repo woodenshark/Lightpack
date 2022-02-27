7eb8804e98b77c6854d2492a99034509da4d3e4f
Patrick Siegler <github@psieg.de>
2022-02-27 01:26:18 +0100

    Update UpdateElevate script to vs2019

--- Software/scripts/win32/build_UpdateElevate.bat
+++ Software/scripts/win32/build_UpdateElevate.bat
@@ -1,2 +1,2 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 MSBuild.exe UpdateElevate\UpdateElevate.sln /p:Configuration=Release
